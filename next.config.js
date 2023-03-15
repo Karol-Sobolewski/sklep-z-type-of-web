467a05aabb3ef5cfc2dca9ec90820b87abc447c5
Karol Sobolewski <karolsobolewski92@gmail.com>
2023-03-15 19:04:18 +0100

    Init project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  compress: true,
+  optimizeFonts: true,
+  experimental: {
+    gzipSize: true,
+  }
 }
 
 module.exports = nextConfig
