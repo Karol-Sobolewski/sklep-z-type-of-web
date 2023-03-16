9a857dd62e2a0c78b09a44a7e55dfa1eb4aa507a
Karol Sobolewski <karolsobolewski92@gmail.com>
2023-03-16 16:55:04 +0100

    Add tailwind CSS

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" >
       <Head />
-      <body>
-        <Main />
+      <body className='bg-gray-50 dark:bg-slate-800  dark:text-white antialiased'>
+        <Main/>
         <NextScript />
       </body>
     </Html>
