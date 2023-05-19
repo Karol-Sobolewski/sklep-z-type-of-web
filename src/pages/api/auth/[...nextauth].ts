640578347be38a158b63d9a02d9166cb685f1b2e
Karol Sobolewski <karolsobolewski92@gmail.com>
2023-05-19 13:13:44 +0200

    Test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ import {
 
 export const authOptions = {
   // Configure one or more authentication providers
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
